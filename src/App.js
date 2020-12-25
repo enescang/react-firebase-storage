1aed447165c9a0a32267d92bf3ed37b5efa0e6c3
kodcanlisi <developerindiee@gmail.com>
2020-12-25 14:54:58 +0300

    Upload file to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { firebaseClient } from "./firebase-client";
 
 function App() {
+  const [image, setImage] = useState('');
+  const [imageUrl, setUrl] = useState();
+
+  const uploadImageToFirebase = async (event) => {
+    event.preventDefault();
+    const ref = firebaseClient.storage().ref('myFolder/myImage.png');
+    const put = await ref.put(image)
+    const url = await put.ref.getDownloadURL();
+    setUrl(url);
+  };
+
+  const changeImage = async (event) => {
+    await setImage(event.target.files[0]);
+    console.log(image)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{imageUrl}</h1>
+      <form
+        method="POST"
+        encType="multipart/form-data"
+        onSubmit={uploadImageToFirebase}
+      >
+        <input
+          type="file"
+          className="filetype"
+          onChange={changeImage}
+        />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
-
 export default App;
